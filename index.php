88a460294acab997b0952837425d9065a322af98
JoelsonDeAbreu <joelsondeabreu@gmail.com>
2011-03-19 18:27:49 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<?php $path = 'public'; ?>
+<!--
+
+Olá. Seja Bem Vindo ao currículo on-line de Joelson de Abreu.
+Não perca seu tempo copiando os arquivos do site. Você encontrará o código completo em http://github.com/joelsondeabreu. :D
+
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Joelson de Abreu</title>
+<link href="<?=$path;?>/css/common.css" rel="stylesheet" type="text/css" />
+<link href="<?=$path;?>/css/theme/style1.css" rel="stylesheet" type="text/css" title="normal" >
+<link href="<?=$path;?>/css/theme/style2.css" rel="stylesheet" type="text/css" title="clean" />
+<script src="<?=$path;?>/js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?=$path;?>/js/styleswitch.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>    
+	<div id="header">
+
+		<ul class="bar-left">
+			<li><a href="#" rel="normal" class="styleswitch">Normal</a></li>
+			<li class="gap"><a href="#" rel="clean" class="styleswitch">Clean</a></li>
+    </ul>
+		
+		<h1>Joelson de Abreu</h1>
+
+		<div id="word">
+			" Este é um pequeno resumo dos conhecimentos no qual adquiri nas instituições, empresas e lugares por onde passei, entretanto um grande profissional não se faz somente de conhecimentos técnicos, mas sim, de experiências vivenciadas e compartilhadas com pessoas, tornando-o a cada dia um profissional melhor, e acima de tudo uma pessoa melhor. " <br><br>
+		</div>
+	</div>
+    
+	<div id="wrapper">  	
+		<div class="content">
+			<div class="subject">
+				Joelson de Abreu
+			</div>
+			<div class="description">
+				<b>E-mail:</b> <img src="public/images/mail.png"><br/>
+				<b>Telefone:</b> (48) 9902-2800<br/>
+				<b>Twitter:</b> <a href="http://www.twitter.com/joelsondeabreu" target="blank">@joelsondeabreu</a><br/>
+				<b>Github:</b> <a href="http://www.github.com/joelsondeabreu" target="blank">joelsondeabreu</a>
+			</div>
+		</div>
+		
+		<div class="content">
+			<div class="subject">
+				Escolaridade
+			</div>
+			<div class="description">
+
+				<h3>Bacharel em Sistemas de Informação<span>Julho de 2012</span></h3>
+				Universidade do Sul de Santa Catarina, Tubarão.<br/>
+				<br/>
+
+				<h3>Técnico de Informática Habilitação em Programação<span>Dezembro de 2007</span></h3>
+				Centro de Educação Profissional Diomício Freitas, Tubarão.<br/>
+
+			</div>
+
+		</div>
+
+    <div class="content">
+			<div class="subject">
+				Cursos
+			</div>
+			<div class="description">
+				
+				<h3>Metaprogramação Ruby<span>Março de 2011</span></h3>
+				Workshop on-line ministrado por Nando Viera.<br/>
+				<br/>
+
+				<h3>Django e Pyton<span>Fevereiro de 2011</span></h3>
+				Curso on-line Welcome to the django ministrado por Henrique Bastos.<br/>
+				<br/>
+
+				<h3>NoSQL<span>Outubro de 2010</span></h3>
+				Curso on-line promovido pela empresa e-Genial Soluções Inteligentes.<br/>
+				<br/>
+				
+				<h3>Ruby On Rails do Básico ao Avançado<span>Agosto de 2010</span></h3>
+				Curso on-line promovido pela empresa e-Genial Soluções Inteligentes.<br/>
+				<br/>
+								
+				<h3>Desenvolvimento de Soluções Java para Web<span>Dezembro de 2009</span></h3>
+				Universidade do Sul de Santa Catarina, Tubarão.<br/>
+				
+			</div>
+		</div>
+
+    <div class="content">
+			<div class="subject">
+				Experiência
+			</div>
+			<div class="description">
+
+				<h3>Engenheiro de Software, Desenvolvedor e DBA<span>Mineoro Indústria Eletrônica (2010 - Presente)</span></h3>
+				Análise e desenvolvimento de aplicações com PHP e Ruby on Rails.<br/>
+				Construção de interfaces com HTML, CSS, Javascript e JQuery.<br/>
+				Configuração e manutenção de servidores linux.<br/>
+				Administração de banco de dados com MySQL.<br/>
+				Gerenciamento da Tecnologia da informação.<br/>
+				<br/>
+				
+				<h3>Desenvolvedor e Analista de Negócios<span>Projedata Soluções Empresariais (2009 - 2010)</span></h3>
+				Desenvolvimento de sistemas ERPs utilizando as linguagens PL/SQL e Java.<br/>
+				Atendimento a clientes em âmbito empresarial procurando solucionar os problemas da empresa.<br/>
+				<br/>
+
+				<h3>Analista e Desenvolvedor Web<span>Gsurf Provedor de Internet (2007 - 2009)</span></h3>
+				Gerenciamento e manutenção do sistema integrado de gestão da empresa utilizando a linguagem PHP, AJAX, MySQL.<br>
+				Gerenciamento e manutenção do sistema web de vendas da empresa.<br/>
+				<br/>
+
+				<h3>Locutor e Técnico de Informática<span>Rádio Comunitária (2004 - 2007)</span></h3>
+				Locução de programas;<br/>
+				Manutenção de hardwares e softwares da empresa.<br/>
+
+			</div>			
+		</div>
+		
+		<div class="content">
+			<div class="subject">
+				Conhecimentos
+			</div>
+			<div class="description">
+
+				<h3>Linguagens</h3>
+				Diariamente trabalho com Ruby, PHP and Javascript; entretanto já utilizei em projetos e trabalhos: Java, Python, PL/SQL, Delphi e Pascal.<br/>
+				<br/>
+				
+				<h3>SGBDs</h3>
+				Mysql, Oracle, Postgres e SQL Server.<br/>
+				<br/>
+
+				<h3>Frameworks</h3>
+				Rails (Ruby), jQuery (Javascript), Django (Python), JSF (Java), Spaghetti (PHP).<br/>				
+				<br/>
+
+				<h3>Sistemas Operacionais</h3>
+				Em meu dia-a-dia uso Mac OS X;<br/>
+				Linux: Fedora, Ubuntu e OpenSuse.<br/>
+				Windows: Windows 98 e posteriores.<br/>
+				<br/>
+
+				<h3>Interface</h3>
+				HTML, XHTML, CSS, Javascript, jQuery, AJAX, W3C (Web Standards).<br/>
+				<br/>
+
+				<h3>Idioma</h3>
+				Português nativo.<br/>
+				Inglês avançado. <br/>
+				<br/>
+
+				<h3>Um pouco mais...</h3>
+				No dia-a-dia procuro utilizar metodologias ágeis.<br/>
+				Uso Git para versionamento de código, tenho experiência também com SVN.<br/>
+				Modelagem de sistemas. (UML)<br/>
+				Manutenção de computadores (hardwares e softwares).<br/>
+			
+			</div>			
+		</div>
+
+		    
+    <div id="linksTitle">
+    	<ul>
+   			<li>E-mail</li> 
+   			<li>Telefone</li> 
+   			<li>Twitter</li> 
+   			<li>Github</li> 
+   		</ul>
+    </div>
+ 		<div id="links">
+			<ul>
+				<li><img src="public/images/mail.png"></li>
+			</ul>
+			<ul>
+     		<li>(48) 9902-2800</li> 
+     </ul>
+ 			<ul>
+	     	<li><a href="http://www.twitter.com/joelsondeabreu" target="blank">@joelsondeabreu</a></li> 
+			</ul>
+			<ul>
+     		<li>
+					<a href="http://www.github.com/joelsondeabreu" target="blank">joelsondeabreu</a>
+				</li> 
+			</ul>
+	</div>
+  <div id="footer">
+		&copy; joelsondeabreu 2011
+  </div>
+</div>
+</body>
+</html>
